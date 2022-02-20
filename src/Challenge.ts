950e0e250a617e131e75aec344292eff3d2405f9
Elec0 <elec08215@gmail.com>
2022-02-20 01:28:15 -0800

    Got most of edit set up. Switches to the view now, saving is being started

--- src/Challenge.ts
+++ src/Challenge.ts
@@ -1,4 +1,5 @@
 import $, { Cash } from "cash-dom";
+import { TAB_OPTIMAL_PATH } from "../dist/bundle";
 import { KEYWORDS } from "./constants";
 import { escapeHtml } from "./utils";
 
@@ -11,26 +12,39 @@ class ChallengeEntry {
     /** What {@link MODES} the challenge applies to. Can be null, which means applies everywhere. */
     mode: string;
     /** How much progression has been completed */
-    progress: number;
+    _progress: number;
     /** The max needed progression */
-    max: number;
+    _max: number;
     /** How many stars the challenge is worth */
-    value: number;
+    _value: number;
     /** What week the challenge is associated with */
-    week: number;
+    _week: number;
     /** If the challenge is part of an event */
     event: boolean;
+    /** 
+     * The order of the challenge in the list, 0 being top.
+     * Also serves as the ID for a challenge, since the order and array index
+     * of the entry in the storage array are the same.
+     */
+    order: number;
 
-    constructor(text: string, progress: number, max: number, value: number, mode?: string, event?: boolean) {
+    private static totalOrder: number = 0;
+
+    constructor(text: string, progress: number, max: number, value: number, mode?: string, event?: boolean, order?: number) {
         this.text = text;
         this.progress = progress;
         this.max = max;
         this.value = value;
-        if(mode !== null)
+        this.order = 0;
+        if(mode !== undefined)
             this.mode = mode;
-        if(event !== null)
+        if(event !== undefined)
             this.event = event;
-        
+        if(order !== undefined)
+            this.order = order;
+        else {
+            this.order = ChallengeEntry.totalOrder++;
+        }
     }
     /**
      * Create dom element with {@link ChallengeRenderer.render}.
@@ -38,6 +52,40 @@ class ChallengeEntry {
     public render() {
         ChallengeRenderer.render(this);
     }
+
+    /** Ensure progress is a number. */
+    public set progress(value: number) {
+        if(!isNaN(value))
+            this._progress = value;
+        else
+            this._progress = 0;
+    }
+    /** Ensure max is a number. */
+    public set max(value: number) {
+        if(!isNaN(value))
+            this._max = value;
+        else
+            this._max = 0;
+    }
+    /** Ensure value is a number. */
+    public set value(value: number) {
+        if(!isNaN(value))
+            this._value = value;
+        else
+            this._value = 0;
+    }
+    /** Ensure week is a number. */
+    public set week(value: number) {
+        if(!isNaN(value))
+            this._week = value;
+        else
+            this._week = 0;
+    }
+
+    public get progress() { return this._progress; }
+    public get max() { return this._max; }
+    public get value() { return this._value; }
+    public get week() { return this._week; }
 }
 
 /**
@@ -60,7 +108,7 @@ class ChallengeEntry {
  class ChallengeRenderer {
     public static render(challenge: ChallengeEntry) {
 
-        let newBar = $("<div>").addClass("challenge-bar challenge-bar-blur")
+        let newBar = $("<div>").addClass("challenge-bar challenge-bar-blur").attr("id", challenge.order.toString());
         let barData = $("<div>").addClass("challenge-bar-data").appendTo(newBar);
         // Create our title element
         $("<div>").addClass("challenge-bar-title")
@@ -75,6 +123,7 @@ class ChallengeEntry {
             .appendTo(barData);
         
         newBar.append(this.starify(challenge));
+        newBar.append(this.setupEditButton(challenge));
 
         $("#challenge-content-area").append(newBar);
     }
@@ -120,10 +169,57 @@ class ChallengeEntry {
                 res.append($("<img>").attr("src", "/res/images/ticket.png").addClass("star-five"));
             }
             return res;
+    }
 
+    /** Create the edit div & setup the click listener for {@link handleEditButtonClick} */
+    private static setupEditButton(challenge: ChallengeEntry): Cash {
+        let res = $("<div>").addClass("edit-icon").attr("id", "")
+            .append($("<img>").attr("src", "/res/images/edit-icon-32x32.png"));
+        res.on("click", (event) => {
+            // The image has pointer events disabled, so we will always get the div out of the click
+            this.handleEditButtonClick(challenge);
+        });
+        return res;
+    }
+
+    /**
+     * Start edit mode. 
+     * Swap all the fields with text boxes, add a save button & listen for tab & enter keys.
+     * Add the IDs of the challenge to each input.
+     * 
+     * On submit, save the data to storage, clear and reload the entire challenge list.
+     */
+    private static handleEditButtonClick(challenge: ChallengeEntry) {
+        let clickedElem = $(`#${challenge.order}`);
+        let cloneElem = $("#challenge-editor").clone().removeAttr("style").attr("id", `edit-${challenge.order}`);
+        let modeSelector = cloneElem.find("select.edit-mode");
+
+        cloneElem.find("div.edit-checkmark").on("click", e => this.handleEditSave(e, challenge));
+
+        // Make the selector have the correct formatting
+        modeSelector.attr("data-chosen", `${challenge.mode}`);
+        clickedElem.empty();
+        clickedElem.append(cloneElem);
+    }
+
+    private static handleEditSave(event: any, challenge: ChallengeEntry) {
+        
+        let cloneElem = $(`#${challenge.order}`);
+        let modeSelector = cloneElem.find("select.edit-mode");
+        let titleText = cloneElem.find("input[for-data='title']");
+        let progressText = cloneElem.find("input[for-data='progress']");
+        let maxText = cloneElem.find("input[for-data='max']");
+        let valueText = cloneElem.find("input[for-data='value']");
+
+        challenge.text = titleText.val() as string;
+        challenge.progress = Number.parseInt(progressText.val() as string);
+        challenge.max = Number.parseInt(maxText.val() as string);
+        challenge.value = Number.parseInt(valueText.val() as string);
+        console.log("Save challenge", challenge);
     }
 }
 
+/** Entry function from navigation */
 export function loadChallenges() {
     let txt = escapeHtml("Play 12 matches as Bloodhound, Seer, or Crypto");
     let testChallenge = new ChallengeEntry("Something event", Math.floor(Math.random()*13), 1000, 200, "BR", true);
@@ -133,6 +229,16 @@ export function loadChallenges() {
         testChallenge = new ChallengeEntry(txt, Math.floor(Math.random()*13), 12, 5, "BR");
         ChallengeRenderer.render(testChallenge);
     }
+}
+
+/** Create the elements for adding new challenges */
+function initChallengeAdder() {
+
+}
+
+/** Get the data from local storage and populate stuff */
+function loadChallengesFromStorage() {
+    
 }
 
 export { ChallengeEntry, ChallengeRenderer };
