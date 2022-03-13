6dc93ef84778e1e611af96be6c265a93b5a2912c
Elec0 <elec08215@gmail.com>
2022-03-13 16:36:44 -0700

    Add verification of challenge existence on load

--- src/storage-helper.ts
+++ src/storage-helper.ts
@@ -55,7 +55,7 @@ export class StorageHelper {
      * Just straight return all the challenges. I guess with TS being itself the objects
      * in the array will be references.
      */
-    public static get challenges() {
+    public static get challenges(): Map<string, ChallengeEntry> {
         return this._challenges;
     }
 
@@ -113,8 +113,7 @@ export class StorageHelper {
 
     /** Explicitly save the current data to storage */
     public static saveToStorage() {
-        console.debug("Saving to storage:", this.challenges);
-        console.debug(JSON.stringify(Array.from(this.challenges)));
+        console.debug("Saving to storage:", this.challenges, this.weekData);
         this.setValue(KEY_CHALLENGES, JSON.stringify(Array.from(this.challenges)));
         this.setValue(KEY_WEEK_DATA, JSON.stringify(this.weekData));
     }
@@ -126,6 +125,9 @@ export class StorageHelper {
         console.debug("Load: raw string:", storChallenges);
         console.debug("Load: raw string:", storWeeks);
 
+        // Keep track of all the challenges as we load them so we can check for orphans
+        let challengeIds: Array<string> = [];
+
         if (storChallenges != null) {
             let jMap: Map<string, any> = new Map(JSON.parse(storChallenges));
             console.debug("Loaded map:", jMap);
@@ -133,15 +135,67 @@ export class StorageHelper {
             jMap.forEach((value, key) => {
                 let newObj: ChallengeEntry = ChallengeEntry.loadFromJson(value);
                 // Saving the data while loading it is really bad, so let's not
-                console.debug("Load object: ", newObj);
                 this.setChallenge(newObj, false);
+                challengeIds.push(key);
             })
+            console.debug("Loaded challenge map:", this.challenges);
         }
 
         if (storWeeks != null) {
             let arr: Array<Array<string>> = JSON.parse(storWeeks);
             this._weekData = arr;
         }
+        // Verify the data, and save it if anything changed.
+        if(this.verifyData(challengeIds)) {
+            this.saveToStorage();
+        }
+    }
+
+    /**
+     * Ensure the loaded challenges all have a week they're assigned to, and that week has
+     * the challenge in it.
+     * 
+     * Don't want to accidentally blow up the storage usage with excess orphans.
+     * @returns boolean, true if any invalid data was found
+     */
+    private static verifyData(challengeIds: Array<string>): boolean {
+        let found: boolean = false;
+        for(let i = 0; i < challengeIds.length; ++i) {
+            // We just constructed the challengeIds array from the loaded list
+            // of challenges, so each of them *will* exist right now.
+            let cWeek = this.challenges.get(challengeIds[i])!.week
+            // Check the week assigned to the challenge to see if the challenge
+            // is present in the week
+            if (this.weekData[cWeek].indexOf(challengeIds[i]) == -1) {
+                // Problem: the challenge exists but isnt in the week it expects to be
+                // Solution: delete the challenge and do a pass through week data at the end
+                console.error("Found orphan challenge! Deleting! This should not happen.", `Expected to be in week ${cWeek}.`, 
+                              this.challenges.get(challengeIds[i]));
+                this.challenges.delete(challengeIds[i]);
+                found = true;
+            }
+        }
+
+        // Now ensure each entry in the weeks arrays exists
+        for(let i = 0; i < this.weekData.length; ++i) {
+            let toRemove: Array<number> = []; // Keep track of what indexes to remove, if any
+
+            for(let j = 0; j < this.weekData[i].length; ++j) {
+                let curId = this.weekData[i][j];
+                if (!this.challenges.has(curId)) {
+                    console.error("Week expects challenge that does not exist!", `Week ${i}[${j}] expects ${curId}`);
+                    toRemove.push(j);
+                    found = true;
+                }
+            }
+            for(let j = 0; j < toRemove.length; ++j) {
+                // Remove the invalid indexes
+                this.weekData.splice(toRemove[j], 1);
+            }
+        }
+
+        // And now we're good, each data point is valid.
+        return found;
     }
 
     /** Abstract the data retrieval method for data retrieval. */
