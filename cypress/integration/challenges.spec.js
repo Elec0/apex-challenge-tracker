f7c9b5b9d2e7da3762c9c9e658dfe8c2baa4feae
Elec0 <elec08215@gmail.com>
2022-04-16 22:32:24 -0700

    Add settings tests

--- cypress/integration/challenges.spec.js
+++ cypress/integration/challenges.spec.js
@@ -1,25 +1,16 @@
 /// <reference types="Cypress" />
+import { enterChallenge } from "../plugins/util-functions";
 
 describe("Create a challenge", () => {
     it("Creates a new challenge: BR, has a name, isn't completed, and has stars", () => {
         cy.visit("/");
-        cy.contains("Challenge").click();
+        cy.get("[data-cy='tab-challenges']").click();
 
         cy.get(".challenge-editor").should("not.exist");
-        cy.contains("New Challenge").click();
-        cy.get(".challenge-editor").should("be.visible");
-
-        cy.contains("Title:").next("span").type("Deal damage as Bangalore.");
-        cy.contains("Progress:")
-            .next("span").clear().type("1") // Progress
-            .next("span").clear().type("10"); // Max
-
-        cy.get("div.star-container > div > span").clear().type("15");
-
-        cy.get("[data-cy='edit-checkmark']").click();
+        enterChallenge("Deal damage as Bangalore.", "1", "10", "15");
     });
     it("Verifies the challenge entered has correct formatting and information", () => {
-        // Getting multiple things is kind of a pain 
+        // Getting multiple things is kind of a pain
         cy.contains("Deal damage as Bangalore.").as("challengeText")
             .find("span").invoke("css", "color")
             .then(function(keywordColor) {
@@ -28,7 +19,11 @@ describe("Create a challenge", () => {
         
         cy.contains("span", "BR");
         cy.contains("1/10");
-
         cy.contains("+15");
+    });
+    it("Deletes the challenge", () => {
+        cy.get("[data-cy='edit-button']").click();
+        cy.get("[data-cy='edit-delete']").click();
+        cy.get(".challenge-bar").should("not.exist");
     });
 });
