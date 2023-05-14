7a68aa307c2124557687909d575b0152d9bf92dd
Elec0 <elec08215@gmail.com>
2023-05-14 12:28:00 -0700

    Added class types as keywords, added Ballistic

--- src/constants.ts
+++ src/constants.ts
@@ -1,17 +1,36 @@
 /** All characters */
-export const LEGENDS: string[] = ["Ash", "Bangalore", "Bloodhound", "Catalyst", "Caustic", "Crypto", 
-                                     "Fuse", "Gibraltar", "Horizon", "Lifeline", "Loba", "Mad Maggie", 
-                                     "Mirage", "Newcastle", "Octane", "Pathfinder", "Rampart", "Revenant", "Seer", 
-                                     "Valkyrie", "Vantage", "Wattson", "Wraith"];
+export const LEGENDS: string[] = ["Ash", "Ballistic", "Bangalore", "Bloodhound", "Catalyst", "Caustic", "Crypto",
+    "Fuse", "Gibraltar", "Horizon", "Lifeline", "Loba", "Mad Maggie",
+    "Mirage", "Newcastle", "Octane", "Pathfinder", "Rampart", "Revenant", "Seer",
+    "Valkyrie", "Vantage", "Wattson", "Wraith"];
+
+/** All legend classes */
+export enum CLASS_TYPES {
+    Assault = "Assault",
+    Skirmisher = "Skirmisher",
+    Recon = "Recon",
+    Support = "Support",
+    Controller = "Controller",
+}
+
+/** Class -> legend mapping */
+export const LEGEND_CLASSES: { [className in CLASS_TYPES]: string[] } = {
+    [CLASS_TYPES.Assault]: ["Bangalore", "Revenant", "Fuse", "Ash", "Mad Maggie", "Ballistic"],
+    [CLASS_TYPES.Skirmisher]: ["Pathfinder", "Wraith", "Mirage", "Octane", "Horizon", "Valkyrie"],
+    [CLASS_TYPES.Recon]: ["Bloodhound", "Crypto", "Seer", "Vantage"],
+    [CLASS_TYPES.Support]: ["Gibraltar", "Lifeline", "Loba", "Newcastle"],
+    [CLASS_TYPES.Controller]: ["Caustic", "Wattson", "Rampart", "Catalyst"],
+};
+
 /** All weapon names */
-export const WEAPON_NAMES: string[] = ["HAVOC", "VK-47 Flatline", "Hemlock", "R-301", "Alternator", "Prowler",
-                                       "R-99", "Volt", "C.A.R.", "Devotion", "L-STAR", "M600 Spitfire", "Rampage",
-                                       "G7 Scout", "Triple Take", "30-30", "Boeck Compound Bow", "Charge Rifle",
-                                       "Longbow", "Kraber", "Sentinel", "EVA-8", "Mastiff", "Mozambique", "Peacekeeper",
-                                       "RE-45", "P2020", "Wingman"];
+export const WEAPON_NAMES: string[] = ["HAVOC", "VK-47 Flatline", "Hemlock", "R-301", "Nemesis", "Alternator", "Prowler",
+    "R-99", "Volt", "C.A.R.", "Devotion", "L-STAR", "M600 Spitfire", "Rampage",
+    "G7 Scout", "Triple Take", "30-30", "Boeck Compound Bow", "Charge Rifle",
+    "Longbow", "Kraber", "Sentinel", "EVA-8", "Mastiff", "Mozambique", "Peacekeeper",
+    "RE-45", "P2020", "Wingman"];
 
 /** All weapon types */
-export const WEAPON_TYPES: string[] = ["assault rifles", "sub machine guns", "light machine guns", 
+export const WEAPON_TYPES: string[] = ["assault rifles", "sub machine guns", "light machine guns",
     "marksman weapons", "sniper rifles", "shotguns", "pistols"];
 
 /** Regex to apply to keywordify numbers */
@@ -27,7 +46,7 @@ export type ModeStrings = keyof typeof MODES;
 export const NumModes: number = Object.keys(MODES).length / 2;
 
 /** Full list of keywords, built from concatenating the subtypes below */
-export const KEYWORDS: string[] = LEGENDS.concat(WEAPON_NAMES, WEAPON_TYPES);
+export const KEYWORDS: string[] = LEGENDS.concat(WEAPON_NAMES, WEAPON_TYPES, Object.keys(CLASS_TYPES));
 
 /** ID correspondig to the tab for Challenges */
 export const TAB_CHALLENGES: string = "Challenges";
