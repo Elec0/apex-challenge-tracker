59114157ccc142a0501e27bdc4d0a034056d2460
Elec0 <elec08215@gmail.com>
2023-07-07 16:11:50 -0700

    Updated Constants tests to actually test something

--- test/test_scripts/ChallengeRenderer.spec.ts
+++ test/test_scripts/ChallengeRenderer.spec.ts
@@ -2,15 +2,16 @@ import expect from 'expect';
 import { ChallengeRenderer } from '../../src/challenge/ChallengeRenderer';
 import { MODES } from '../../src/constants';
 
-
-test("Generate proper mode span", () => {
-    let result = ChallengeRenderer.modeify(MODES.BR);
-    expect(result.attr("class")).toContain("cb-type");
-    expect(result.attr("class")).toContain("type-br")
-    expect(result.text()).toContain("BR")
-});
-test("Properly Keywordify", () => {
-    let toTest = ChallengeRenderer.keywordify("Time to kill xxx with Seer and a light machine guns");
-    expect(toTest.html()).toContain("<span class=\"keyword\">Seer</span>");
-    expect(toTest.html()).toContain("<span class=\"keyword\">light machine guns</span>");
+describe("ChallengeRenderer", () => {
+    test("Generate proper mode span", () => {
+        let result = ChallengeRenderer.modeify(MODES.BR);
+        expect(result.attr("class")).toContain("cb-type");
+        expect(result.attr("class")).toContain("type-br")
+        expect(result.text()).toContain("BR")
+    });
+    test("Properly Keywordify", () => {
+        let toTest = ChallengeRenderer.keywordify("Time to kill xxx with Seer and a light machine guns");
+        expect(toTest.html()).toContain("<span class=\"keyword\">Seer</span>");
+        expect(toTest.html()).toContain("<span class=\"keyword\">light machine guns</span>");
+    });
 });
